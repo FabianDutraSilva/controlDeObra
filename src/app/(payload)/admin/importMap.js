3bb3ad90be8bd9fff4e9df3e3126854d3c9bd591
Fabián Dutra <fabiandutrasilva@gmail.com>
2025-07-22 00:13:28 -0300

    feat: apply formatted number on admin view

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,5 +1,5 @@
-
+import { NumberComponent as NumberComponent_cc8ffb4d72cccd0f4440f5702b1e4139 } from '@nouance/payload-better-fields-plugin/Number/client'
 
 export const importMap = {
-
+  "@nouance/payload-better-fields-plugin/Number/client#NumberComponent": NumberComponent_cc8ffb4d72cccd0f4440f5702b1e4139
 }
